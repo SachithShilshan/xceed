723501412690ae5d8ec60479030a464a5e3f6139
Sachith Shilshan <sachith.edu6021@gmail.com>
2025-10-29 07:27:32 +0530

    int

--- xceed-digital/vite.config.js
+++ xceed-digital/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // important for gh-pages relative paths
+  base: '/xceed/' // important for gh-pages relative paths
 })
